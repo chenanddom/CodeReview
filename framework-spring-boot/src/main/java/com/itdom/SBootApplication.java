1a7465d76d6ebf46899463fdee22dccba3ba2871
chenanddom <772571631@qq.com>
2022-03-30 11:22:19 +0800

    提交springboot项目的修改

--- framework-spring-boot/src/main/java/com/itdom/SBootApplication.java
+++ framework-spring-boot/src/main/java/com/itdom/SBootApplication.java
@@ -2,10 +2,12 @@ package com.itdom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SBootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SBootApplication.class,args);
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(SBootApplication.class, args);
+        configurableApplicationContext.getBean("");
     }
 }
