400b061af9d5722639c8d31639216450fbfcdf8a
chenanddom <772571631@qq.com>
2022-08-06 19:38:53 +0800

    提交kafka生产者发送的原理、异步发送消息的笔记和相关的代码文件和图片文件

---
+++ mq-kafka/kafka-async-send/src/main/java/com/itdom/kafka/CustomProducer.java
@@ -0,0 +1,37 @@
+package com.itdom.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class CustomProducer {
+
+    public static void main(String[] args) {
+
+        // 0 配置
+        Properties properties = new Properties();
+
+        // 连接集群 bootstrap.servers
+//        properties.put(/*ProducerConfig.BOOTSTRAP_SERVERS_CONFIG*/"bootstrap.servers","kafa-node1:9092,kafa-node2:9092,kafa-node3:9092");
+        properties.put(/*ProducerConfig.BOOTSTRAP_SERVERS_CONFIG*/"bootstrap.servers","192.168.0.190:9092,192.168.0.191:9092,192.168.0.192ss:9092");
+
+        // 指定对应的key和value的序列化类型 key.serializer
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        // 1 创建kafka生产者对象
+        // "" hello
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+
+        // 2 发送数据
+        for (int i = 0; i < 100; i++) {
+            kafkaProducer.send(new ProducerRecord<String,String>("first","kafkamessage"+i));
+        }
+
+        // 3 关闭资源
+        kafkaProducer.close();
+    }
+}
