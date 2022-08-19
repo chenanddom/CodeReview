8a9f7928a837bd818aaa45327d26307abd843338
chenanddom <772571631@qq.com>
2022-08-19 11:04:50 +0800

    提交acl的笔记、代码和相关的图片文件

--- long-connection-mqtt/mqtt-http-auth/src/main/java/com/itdom/controller/AuthController.java
+++ long-connection-mqtt/mqtt-http-auth/src/main/java/com/itdom/controller/AuthController.java
@@ -5,11 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping("/mqtt")
@@ -26,7 +27,34 @@ public class AuthController {
                                    @RequestParam("password") String password) {
         logger.debug("开始认证clientid:{},username:{},password:{}", clientid, username, password);
 
-        return authService.authUser(clientid,username,password);
+        return authService.authUser(clientid, username, password);
+    }
+
+    @PostMapping("/superuser")
+    public ResponseEntity superUser(@RequestParam("clientid") String clientid,
+                                       @RequestParam("username") String username) {
+        return authService.superUser(clientid, username);
+    }
+
+    @PostMapping("/acl")
+    public ResponseEntity acl(@RequestParam("access") int access,
+                              @RequestParam("username") String username,
+                              @RequestParam("clientid") String clientid,
+                              @RequestParam("ipaddr") String ipaddr,
+                              @RequestParam("topic") String topic,
+                              @RequestParam("mountpoint") String mountpoint) {
+        logger.info("EMQX发起客户端操作授权查询请求,access={},username={},clientid={},ipaddr= {},topic={},mountpoint={}",
+                access, username, clientid, ipaddr, topic, mountpoint);
+        if (username.equals("chendom") && topic.equals("testtopic/#") && access == 1) {
+            logger.info("客户端{}有权限订阅{}", username, topic);
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        if (username.equals("emq-client-id-001") && topic.equals("testtopic/123") && access == 2) {
+            logger.info("客户端{}有权限向{}发布消息", username, topic);
+            return new ResponseEntity(null, HttpStatus.OK);
+        }
+        logger.info("客户端{},username={},没有权限对主题{}进行{}操作", clientid, username, topic, access == 1 ? "订阅" : "发布");
+        return new ResponseEntity(HttpStatus.UNAUTHORIZED);//无权限
     }
 
 }
--- long-connection-mqtt/mqtt-http-auth/src/main/java/com/itdom/service/AuthService.java
+++ long-connection-mqtt/mqtt-http-auth/src/main/java/com/itdom/service/AuthService.java
@@ -1,5 +1,8 @@
 package com.itdom.service;
 
+import com.itdom.controller.AuthController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,10 +10,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class AuthService {
+    private static final Logger logger = LoggerFactory.getLogger(AuthService.class);
     private static final Map<String, String> map = new ConcurrentHashMap<String, String>(16) {{
         put("chendom", "chendom@123");
         put("admin", "public");
@@ -27,5 +32,18 @@ public class AuthService {
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    public ResponseEntity superUser(String clientid, String username) {
+        logger.info("emqx 查询是否是超级用户,clientid={},username={}", clientid, username);
+        if (clientid.contains("admin") || username.contains("admin")) {
+            logger.info("用户{}是超级用户", username);
+            //是超级用户
+            return new ResponseEntity<Object>(HttpStatus.OK);
+        } else {
+            logger.info("用户{}不是超级用户", username);
+            //不是超级用户
+            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
